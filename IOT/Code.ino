70baeab35e3084f292f17482f7ed4d53f194077a
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-09-10 22:39:53 -0300

    Save code

---
+++ IOT/Code.ino
@@ -0,0 +1,139 @@
+// Read NUID tag from card.
+
+#include <SPI.h>
+#include <MFRC522.h>
+#include <ESP32Servo.h>
+
+#define SS_PIN 5
+#define RST_PIN 2
+
+#define SERVO_PIN 25
+#define GREEN_PIN 12
+#define RED_PIN 32
+#define BUZZER_PIN 13
+ 
+Servo servo;
+int angle = 0;
+
+MFRC522 rfid(SS_PIN, RST_PIN); // Instance of the reader object.
+MFRC522::MIFARE_Key key; 
+
+byte nuidPICC[4]; // Init array that will store new NUID.
+
+void setup() { 
+  Serial.begin(9600);
+  SPI.begin(); // Init SPI bus.
+  rfid.PCD_Init(); // Init MFRC522.
+
+	ESP32PWM::allocateTimer(0);
+	ESP32PWM::allocateTimer(1);
+	ESP32PWM::allocateTimer(2);
+	ESP32PWM::allocateTimer(3);
+	servo.setPeriodHertz(50);
+	servo.attach(SERVO_PIN, 500, 2400);
+
+  pinMode(GREEN_PIN, OUTPUT);
+  pinMode(RED_PIN, OUTPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
+
+  // For read and write operations we prepare a security key.
+  // Here all six bytes of the key are set to 0xFF.
+  for (byte i = 0; i < 6; i++) {
+    key.keyByte[i] = 0xFF;
+  }
+
+  Serial.println(F("This code scan the MIFARE Classsic NUID."));
+  Serial.print(F("Using the following key:"));
+  printHex(key.keyByte, MFRC522::MF_KEY_SIZE);
+}
+ 
+void loop() {
+  digitalWrite(RED_PIN, HIGH);
+  // Reset the loop if no new card present on the sensor/reader.
+  // This saves the entire process when idle.
+  if (!rfid.PICC_IsNewCardPresent())
+    return;
+
+  // Verify if the NUID has been readed
+  if (!rfid.PICC_ReadCardSerial())
+    return;
+
+  digitalWrite(RED_PIN, LOW);
+
+  digitalWrite(GREEN_PIN, HIGH);
+  digitalWrite(BUZZER_PIN, HIGH);
+  delay(500);
+  digitalWrite(BUZZER_PIN, LOW);
+
+  for (angle = 0; angle <= 120; angle += 2) {
+    servo.write(angle);
+    delay(15);
+  }
+
+  delay(3000);
+
+  digitalWrite(GREEN_PIN, LOW);
+  digitalWrite(RED_PIN, HIGH);
+  for (angle = 120; angle >=0; angle -= 2) {
+    servo.write(angle);
+    delay(15);
+  }
+
+  Serial.print(F("PICC type: "));
+  MFRC522::PICC_Type piccType = rfid.PICC_GetType(rfid.uid.sak);
+  Serial.println(rfid.PICC_GetTypeName(piccType));
+
+  // Check is the PICC of Classic MIFARE type
+  if (piccType != MFRC522::PICC_TYPE_MIFARE_MINI &&  
+    piccType != MFRC522::PICC_TYPE_MIFARE_1K &&
+    piccType != MFRC522::PICC_TYPE_MIFARE_4K) {
+    Serial.println(F("Your tag is not of type MIFARE Classic."));
+    return;
+  }
+
+  if (rfid.uid.uidByte[0] != nuidPICC[0] || 
+    rfid.uid.uidByte[1] != nuidPICC[1] || 
+    rfid.uid.uidByte[2] != nuidPICC[2] || 
+    rfid.uid.uidByte[3] != nuidPICC[3] ) {
+    Serial.println(F("A new card has been detected."));
+
+    // Store NUID into nuidPICC array
+    for (byte i = 0; i < 4; i++) {
+      nuidPICC[i] = rfid.uid.uidByte[i];
+    }
+   
+    Serial.println(F("The NUID tag is:"));
+
+    Serial.print(F("In hex: "));
+    printHex(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+
+    Serial.print(F("In dec: "));
+    printDec(rfid.uid.uidByte, rfid.uid.size);
+    Serial.println();
+  }
+  else
+  {
+    Serial.println(F("Card read previously."));
+  }
+
+  // Halt PICC
+  rfid.PICC_HaltA();
+
+  // Stop encryption on PCD
+  rfid.PCD_StopCrypto1();
+}
+
+void printHex(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(buffer[i] < 0x10 ? " 0" : " ");
+    Serial.print(buffer[i], HEX);
+  }
+}
+
+void printDec(byte *buffer, byte bufferSize) {
+  for (byte i = 0; i < bufferSize; i++) {
+    Serial.print(' ');
+    Serial.print(buffer[i], DEC);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # karking
+
 Karking
+
+## Erro na lib MFRC522 + ESP32
+
+in the file: /Arduino/libraries/MFRC522/src/MFRC522Extended.cpp
+
+Line: 824 and 847
+replace: if (backData && (backLen > 0))
+by : if (backData && backLen != nullptr)
