e6ae6851f0e9bf1e49b181fa52ceded7919809a8
ZaqueuCavalcante <zaqueudovale@gmail.com>
2024-11-06 19:36:27 -0300

    Try local time

--- Back/Features/GetVehicles/GetVehiclesController.cs
+++ Back/Features/GetVehicles/GetVehiclesController.cs
@@ -10,6 +10,14 @@ public class GetVehiclesController(KarkingDbContext ctx) : ControllerBase
     {
         var sessions = await ctx.Sessions.OrderByDescending(x => x.EntryAt).ToListAsync();
 
-        return Ok(sessions);
+        return Ok(sessions.ConvertAll(x => new {
+            Plate = x.Plate,
+            EntryAt = x.EntryAt.ToLocalTime(),
+            PayToken = x.PayToken,
+            PaidAmount = x.PaidAmount,
+            PaidAt = x.PaidAt != null ? x.PaidAt.Value.ToLocalTime() : x.PaidAt,
+            ExitLimit = x.ExitLimit != null ? x.ExitLimit.Value.ToLocalTime() : x.ExitLimit,
+            ExitAt = x.ExitAt != null ? x.ExitAt.Value.ToLocalTime() : x.ExitAt,
+        }));
     }
 }
