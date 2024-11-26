a964edb16d96ef2811e7b0f9d89b20dd042ee614
Wendell Marinho <wtmarinho55@gmail.com>
2024-11-26 11:07:23 -0300

    feat: configuração de rotas

--- Front/src/main.tsx
+++ Front/src/main.tsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import { BrowserRouter} from 'react-router'
+import MainRoutes from './routes'
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+  <BrowserRouter>
+    <MainRoutes></MainRoutes>
+  </BrowserRouter>,
 )
