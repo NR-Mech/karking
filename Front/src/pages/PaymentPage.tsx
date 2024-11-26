0258aa3d208ac26309f4b6eb957edf7744fb09f9
Wendell Marinho <wtmarinho55@gmail.com>
2024-11-26 11:25:25 -0300

    feat: alteração do nome da página

--- Front/src/pages/PaymentPage.tsx
+++ Front/src/pages/PaymentPage.tsx
@@ -3,8 +3,14 @@ import Container from "../components/Container"
 import Input from "../components/Input"
 import Button from "../components/Button"
 import DisplayPayment from "../components/DisplayPayment"
+import { useEffect } from "react"
 
 export default function PaymentPage(){
+
+  useEffect(() => {
+    document.title = "Pagamento | Embarc Parking";
+  }, []);
+
   return(
     <Container>
       <Title/>
