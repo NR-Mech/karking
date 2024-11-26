6c659f7a5e2a836bdffdb00f5129f4730cac8186
Wendell Marinho <wtmarinho55@gmail.com>
2024-11-26 11:25:52 -0300

    cursor pointer

--- Front/src/components/Button.tsx
+++ Front/src/components/Button.tsx
@@ -6,7 +6,7 @@ type buttonProps = {
 export default function Button({content, onClick}: buttonProps){
   return(
     <div>
-      <button onClick={onClick} className="bg-gradient-to-b from-[#E67D1A] to-[#CB5C14] text-[#411307] font-poppins font-semibold text-base tracking-wide py-2 px-6 rounded-[5px] uppercase">{content}</button>
+      <button onClick={onClick} className="bg-gradient-to-b from-[#E67D1A] to-[#CB5C14] text-[#411307] font-poppins font-semibold text-base tracking-wide py-2 px-6 rounded-[5px] uppercase cursor-pointer">{content}</button>
     </div>
   )
 }
