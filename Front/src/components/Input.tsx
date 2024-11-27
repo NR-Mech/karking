ad657749d20e30b0503f4667f14d08dd102f9651
Wendell Marinho <wtmarinho55@gmail.com>
2024-11-27 10:59:10 -0300

    fix: input deve possuir no máximo 7 caracteres

--- Front/src/components/Input.tsx
+++ Front/src/components/Input.tsx
@@ -12,7 +12,7 @@ export default function Input({ content }: InputProps) {
         <h6 className="font-poppins text-white text-xs font-semibold text-center">BRASIL</h6>
         <img src={brazilFlag} alt="" className='absolute top-[2px] right-1'/>
       </div>
-      <input placeholder="BRA3R52" type="text" name="input" id="input" className="w-52 py-3 text-center outline-none font-pt-sans-narrow text-[1.25rem] uppercase rounded-b-[5px]" />
+      <input placeholder="BRA3R52" type="text" maxLength={7} name="input" id="input" className="w-52 py-3 text-center outline-none font-pt-sans-narrow text-[1.25rem] uppercase rounded-b-[5px]" />
     </div>
   );
 }
