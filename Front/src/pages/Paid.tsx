ceb23c80fd2e507f8a54178b94636289b805556c
Wendell Marinho <wtmarinho55@gmail.com>
2024-12-04 10:10:35 -0300

    feat: adicionado pop-up

--- Front/src/pages/Paid.tsx
+++ Front/src/pages/Paid.tsx
@@ -1,11 +1,13 @@
 import { useNavigate } from "react-router";
 import Button from "../components/Button";
+import PopupCare from "../components/PopupCare";
 
 export function Paid() {
   const navigate = useNavigate();
   return (
     <div className="flex flex-col justify-center items-center gap-5">
       <h1 className="text-white text-2xl font-bold">Pagamento Efetuado!</h1>
+      <PopupCare/>
       <Button content="Voltar" onClick={() => navigate("/")}/>
     </div>
   )
