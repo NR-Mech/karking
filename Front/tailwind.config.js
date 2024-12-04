697beca3a481b3c52201705434c494f0ddb132ff
Wendell Marinho <wtmarinho55@gmail.com>
2024-12-04 10:07:22 -0300

    feat: instalação do shadcn

--- Front/tailwind.config.js
+++ Front/tailwind.config.js
@@ -1,17 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
+    darkMode: ["class"],
+    content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      fontFamily: {
-        'pt-sans-narrow': ['"PT Sans Narrow"', 'sans-serif'],
-        'poppins': ['"Poppins"', 'sans-serif'],
-      },
-    },
+  	extend: {
+  		fontFamily: {
+  			'pt-sans-narrow': ['PT Sans Narrow"', 'sans-serif'],
+  			poppins: ['Poppins"', 'sans-serif']
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		},
+  		colors: {}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 }
 
